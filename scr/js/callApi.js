bdc08ea5622b4c8a48cb84f906237f2177d3622c
ACarmonaTS <ts.acarmona@outlook.com>
2023-01-07 00:06:41 -0600

    Pruebas con la API seleccionada

--- scr/js/callApi.js
+++ scr/js/callApi.js
@@ -2,26 +2,24 @@ const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=mxn&orde
 const urlPart1 = "https://api.coingecko.com/api/v3/coins/bitcoin/history?date=";
 const urlPart2 = "&localization=mxn";
 const coins = async() => {
-     //NO MOVER
-    //Para url (HOY)
-    // const res = await fetch(url);
-    // const data = await res.json();
-    // console.log(data);
-    // const dato = data[0].current_price;
-    // document.getElementById("showValue").value = "$" + dato + " M.N.";
-
-    //NO MOVER
-    //Para url2 (BUSCAR POR FECHA)
+    //Search for date
     day = document.getElementById("daysInput").value;
     month = document.getElementById("monthsInput").value;
     year = document.getElementById("yearsInput").value;
-    const urlDate = urlPart1 + day + "-" + month + "-" + year + urlPart2; 
-    const res = await fetch(urlDate);
-    const data = await res.json();
-    console.log(data);
-    console.log(data.market_data.current_price.mxn);
-    const dato = data.market_data.current_price.mxn;
-    document.getElementById("showValue").value = "$" + Number.parseFloat(dato).toFixed(2).toString() + " M.N.";
+    let urlDate = urlPart1 + day + "-" + month + "-" + year + urlPart2; 
+    let res = await fetch(urlDate);
+    let data = await res.json();
+    let dato = data.market_data.current_price.mxn;
+    document.getElementById("showValuePast").value = "$" + Number.parseFloat(dato).toFixed(2).toString() + " M.N.";
 }
 
+const coinNow = async() => {
+    //Current value of coins
+    let res = await fetch(url);
+    let data = await res.json();
+    let dato = data[0].current_price;
+    document.getElementById("showValueNow").value = "$" + Number.parseFloat(dato).toFixed(2).toString() + " M.N.";
+}
+coinNow()
+setInterval(coinNow, 60000);
 // coins(); NO MOVER
