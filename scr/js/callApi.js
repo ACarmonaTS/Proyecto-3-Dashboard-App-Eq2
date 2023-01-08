e2f996b2b62caac9266f4e855caffc4593919a6d
ACarmonaTS <ts.acarmona@outlook.com>
2023-01-07 21:59:27 -0600

    Cambios de pedido de datos a API

--- scr/js/callApi.js
+++ scr/js/callApi.js
@@ -1,6 +1,9 @@
+import * as graphs from "./graphs.js"
+
 const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=mxn&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 const urlPart1 = "https://api.coingecko.com/api/v3/coins/bitcoin/history?date=";
 const urlPart2 = "&localization=mxn";
+
 const coins = async() => {
     //Search for date
     day = document.getElementById("daysInput").value;
@@ -11,6 +14,7 @@ const coins = async() => {
     let data = await res.json();
     let dato = data.market_data.current_price.mxn;
     document.getElementById("showValuePast").value = "$" + Number.parseFloat(dato).toFixed(2).toString() + " M.N.";
+    graphs.graphses();
 }
 
 const coinNow = async() => {
@@ -23,3 +27,4 @@ const coinNow = async() => {
 coinNow()
 setInterval(coinNow, 60000);
 // coins(); NO MOVER
+
