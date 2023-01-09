32d10f0363d2b4c8410ebb536f16a8bfde41eed7
ACarmonaTS <ts.acarmona@outlook.com>
2023-01-09 11:58:06 -0600

    Problemas con onclick

--- scr/js/callApi.js
+++ scr/js/callApi.js
@@ -1,30 +1,26 @@
-import * as graphs from "./graphs.js"
-
 const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=mxn&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 const urlPart1 = "https://api.coingecko.com/api/v3/coins/bitcoin/history?date=";
 const urlPart2 = "&localization=mxn";
 
-const coins = async() => {
-    //Search for date
-    day = document.getElementById("daysInput").value;
-    month = document.getElementById("monthsInput").value;
-    year = document.getElementById("yearsInput").value;
-    let urlDate = urlPart1 + day + "-" + month + "-" + year + urlPart2; 
+async function coinsPast(){
+    const day = document.getElementById("daysInput").value;
+    const month = document.getElementById("monthsInput").value;
+    const year = document.getElementById("yearsInput").value;
+    const urlDate = urlPart1 + day + "-" + month + "-" + year + urlPart2; 
     let res = await fetch(urlDate);
     let data = await res.json();
     let dato = data.market_data.current_price.mxn;
     document.getElementById("showValuePast").value = "$" + Number.parseFloat(dato).toFixed(2).toString() + " M.N.";
-    graphs.graphses();
 }
 
-const coinNow = async() => {
+const coinsNow = async() => {
     //Current value of coins
     let res = await fetch(url);
     let data = await res.json();
     let dato = data[0].current_price;
+    console.log(dato)
     document.getElementById("showValueNow").value = "$" + Number.parseFloat(dato).toFixed(2).toString() + " M.N.";
 }
-coinNow()
-setInterval(coinNow, 60000);
-// coins(); NO MOVER
 
+coinsNow();
+setInterval(coinsNow, 60000);
--- scr/js/graphs.js
+++ scr/js/graphs.js
@@ -1,22 +1,19 @@
-export function graphses(){
-    const ctx = document.getElementById('graph');
-
-    new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-        y: {
-            beginAtZero: true
-        }
-        }
+const ctx = document.getElementById('graph');
+new Chart(ctx, {
+type: 'bar',
+data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+    label: '# of Votes',
+    data: [12, 19, 3, 5, 2, 3],
+    borderWidth: 1
+    }]
+},
+options: {
+    scales: {
+    y: {
+        beginAtZero: true
     }
-    });
+    }
 }
+});
