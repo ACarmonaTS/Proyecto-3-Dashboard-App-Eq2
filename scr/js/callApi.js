71858e9ded84bb167a898dc9014b3a6515a87b70
ACarmonaTS <ts.acarmona@outlook.com>
2023-01-13 00:02:57 -0600

    Correcciones para graficar

--- scr/js/callApi.js
+++ scr/js/callApi.js
@@ -2,27 +2,28 @@ const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=mxn&orde
 const urlPart1 = "https://api.coingecko.com/api/v3/coins/bitcoin/history?date=";
 const urlPart2 = "&localization=mxn";
 
-document.getElementById("search").addEventListener("click", coinsPast );
+document.getElementById("search").addEventListener("click", coinsPast);
 
-export async function coinsPast(){
-    const day = document.getElementById("daysInput").value;
-    const month = document.getElementById("monthsInput").value;
-    const year = document.getElementById("yearsInput").value;
-    const urlDate = urlPart1 + day + "-" + month + "-" + year + urlPart2; 
-    let res = await fetch(urlDate);
-    let data = await res.json();
-    let dato = data.market_data.current_price.mxn;
-    document.getElementById("showValuePast").value = "$" + Number.parseFloat(dato).toFixed(2).toString() + " M.N.";
+async function coinsPast() {
+  const day = document.getElementById("daysInput").value;
+  const month = document.getElementById("monthsInput").value;
+  const year = document.getElementById("yearsInput").value;
+  const urlDate = urlPart1 + day + "-" + month + "-" + year + urlPart2;
+  let res = await fetch(urlDate);
+  let data = await res.json();
+  let dato = data.market_data.current_price.mxn;
+  document.getElementById("showValuePast").value =
+    "$" + Number.parseFloat(dato).toFixed(2).toString() + " M.N.";
 }
 
-const coinsNow = async() => {
-    //Current value of coins
-    let res = await fetch(url);
-    let data = await res.json();
-    let dato = data[0].current_price;
-    console.log(dato)
-    document.getElementById("showValueNow").value = "$" + Number.parseFloat(dato).toFixed(2).toString() + " M.N.";
-}
+const coinsNow = async () => {
+  //Current value of coins
+  let res = await fetch(url);
+  let data = await res.json();
+  let dato = data[0].current_price;
+  document.getElementById("showValueNow").value =
+    "$" + Number.parseFloat(dato).toFixed(2).toString() + " M.N.";
+};
 
 coinsNow();
-setInterval(coinsNow, 60000);
+setInterval(coinsNow, 1800000);
